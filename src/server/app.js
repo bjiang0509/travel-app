b8d6b324e36f8ce9f8fd96d3d19c0d0283ce170a
bjiang0509 <86031227+bjiang0509@users.noreply.github.com>
2022-03-24 00:16:43 -0700

    Delete src/server directory

--- src/server/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-// Start up an instance of app
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send({msg: "server test"});
-});
-
-export {app};
